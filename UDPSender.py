59cbecec117fd5a7748c238855e51c5cfe93aa15
Russ Thom <r.thom.09@aberdeen.ac.uk>
2014-03-02 21:27:37 +0000

    UDPSender class added, for converting and sending CAN bus messages over UDP (extends listener for working in current python-can framework)

---
+++ UDPSender.py
@@ -0,0 +1,29 @@
+from can import Listener
+from can import Message
+import socket
+
+class UDPSender(Listener):
+    dataConvert = {"0x600": {"String":"RPM:",
+                             "Slot":0},
+                   "0x601": {"String":"OIL:",
+                             "Slot":2}}
+    
+    def __init__(self, IP="10.0.0.4", PORT=5555):
+        self.ip = IP
+        self.port = PORT
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        
+    def on_message_received(self, msg):
+        udpMessage = self.can_to_udp_message(msg)
+        self.sock.sendto(udpMessage.encode(), (self.ip, self.port))
+        
+    def can_to_udp_message(self, msg):
+        hexId = msg.arbritation_id
+        dataId = self.dataConvert[hexId]["String"]
+        dataSlot = self.dataConvert[hexId]["Slot"]
+        data = (msg.data[dataSlot] << 8) + msg.data[dataSlot + 1]
+        udpMessage = dataId + data
+        return udpMessage
+        
+    def __del__(self):
+        self.sock.close()
