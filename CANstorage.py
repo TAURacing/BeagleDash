d10721bea602db1b128df783af0005c43d6a2f36
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-05-10 17:29:51 +0100

    Now using tables per sequence for storing data.

--- CANstorage.py
+++ CANstorage.py
@@ -5,6 +5,7 @@ from tinydb import TinyDB, where
 
 class CanStorage:
     __data_base = TinyDB
+    __current_sequence_table = TinyDB.table
     __current_sequence = None
     __max_sequence = None
     __ready_to_store = False
@@ -12,13 +13,16 @@ class CanStorage:
     def __init__(self, a_file_path):
         self.__data_base = TinyDB(a_file_path)
         # Check if we have a current sequence stored in the filemajigger
-        sequence_check = self.__data_base.search(where('sequence'))
+        sequence_table = self.__data_base.table('sequence_counter')
+        sequence_check = sequence_table.search(where('sequence'))
+        print sequence_check
         # If a previous sequence exist we increment the max by one
         if sequence_check:
             self.__max_sequence = max(sequence_check)['sequence']
         # If this is the first entry set current sequence to 0
         else:
             self.__max_sequence = 0
+        print self.__max_sequence
 
     def test_this_thing(self):
         print self.__data_base.all()
@@ -28,28 +32,28 @@ class CanStorage:
     def __init_storage(self):
         self.__current_sequence = self.__max_sequence + 1
         # Store the current sequence to db for next time the file is opened
-        self.__data_base.insert({'sequence': self.__current_sequence})
+        sequence_table = self.__data_base.table('sequence_counter')
+        sequence_table.insert({'sequence': self.__current_sequence})
+        # Create new table entry for this sequence
+        sequence_name = 'sequence' + str(self.__current_sequence)
+        self.__current_sequence_table = self.__data_base.table(sequence_name)
         self.__ready_to_store = True
 
     def store(self, a_dict_entry):
         if not self.__ready_to_store:
             self.__init_storage()
-        # Add the current sequence as key in dictionary we're storing
-        a_dict_entry['sequence'] = self.__current_sequence
         # Extract the data_id key
         data_key = a_dict_entry['data_id']
         # Remove the data_id key from the dictionary
         a_dict_entry.pop(data_key, 0)
         # Store the passed dictionary with its key being the data_id field
-        self.__data_base.insert({data_key: a_dict_entry})
+        self.__current_sequence_table.insert({data_key: a_dict_entry})
 
     def load(self, a_sequence_number, a_key):
-        key_list = self.__data_base.search(where(a_key))
-        sequence_list = list()
-        for item in key_list:
-            if item[a_key]['sequence'] == a_sequence_number:
-                sequence_list.append(item[a_key])
-        return sequence_list
+        sequence_name = 'sequence' + str(a_sequence_number)
+        selected_table = self.__data_base.table(sequence_name)
+        key_list = selected_table.search(where(a_key))
+        return key_list
 
     def get_max_sequence(self):
         return self.__max_sequence
@@ -67,10 +71,10 @@ somedict['timestamp'] = 32434
 somedict2 = dict(somedict)
 somedict2['timestamp'] = 46666
 
-#testStore.store(somedict)
-#testStore.store(somedict2)
+testStore.store(somedict)
+testStore.store(somedict2)
 
-#testStore.test_this_thing()
+testStore.test_this_thing()
 
 extracted_list = testStore.load(1, 'rpm')
 print extracted_list
