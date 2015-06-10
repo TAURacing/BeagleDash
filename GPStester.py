800636431a00f671238854d0ba62ef16d348644d
thisisG <geir.istad.11@aberdeen.ac.uk>
2015-06-10 15:27:58 +0100

    testing new threading system

--- GPStester.py
+++ GPStester.py
@@ -1,61 +1,55 @@
 import os
-from gps import *
-from time import *
+import gps
+import time
 import time
 import threading
-
-gpsd = None #seting the global variable
-
-os.system('clear') #clear the terminal (optional)
 
 class GpsPoller(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
-        global gpsd #bring it in scope
-        gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-        self.current_value = None
-        self.running = True #setting the thread running to true
+        self.gpsd = gps.gps(mode=WATCH_ENABLE)
+        self.current_value = None # ???
+        self.running = True
 
     def run(self):
-        global gpsd
-        while gpsp.running:
-            if gpsd.next() != -1:
+        while self.running:
+            if self.gpsd.next() != -1:
                 print('got data')
-            elif: gpsd.next() == 1:
+                print('lat: ' + str(self.gpsd.fix.latitude))
+            elif: self.gpsd.next() == 1:
                 print('got -1')
 
-if __name__ == '__main__':
-    gpsp = GpsPoller() # create the thread
-    try:
-        gpsp.start() # start it up
-        while True:
-            #It may take a second or two to get good data
-            #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
+gps_thread = GpsPoller() # create the thread
+try:
+    gps_thread.start() # start it up
+    while True:
+        #It may take a second or two to get good data
+        #print gpsd.fix.latitude,', ',gpsd.fix.longitude,'  Time: ',gpsd.utc
 
-            os.system('clear')
+        #os.system('clear')
+#
+        #print
+        #print ' GPS reading'
+        #print '----------------------------------------'
+        #print 'latitude    ' , gpsd.fix.latitude
+        #print 'longitude   ' , gpsd.fix.longitude
+        #print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
+        #print 'altitude (m)' , gpsd.fix.altitude
+        #print 'eps         ' , gpsd.fix.eps
+        #print 'epx         ' , gpsd.fix.epx
+        #print 'epv         ' , gpsd.fix.epv
+        #print 'ept         ' , gpsd.fix.ept
+        #print 'speed (m/s) ' , gpsd.fix.speed
+        #print 'climb       ' , gpsd.fix.climb
+        #print 'track       ' , gpsd.fix.track
+        #print 'mode        ' , gpsd.fix.mode
+        #print
+        #print 'sats        ' , gpsd.satellites
 
-            print
-            print ' GPS reading'
-            print '----------------------------------------'
-            print 'latitude    ' , gpsd.fix.latitude
-            print 'longitude   ' , gpsd.fix.longitude
-            print 'time utc    ' , gpsd.utc,' + ', gpsd.fix.time
-            print 'altitude (m)' , gpsd.fix.altitude
-            print 'eps         ' , gpsd.fix.eps
-            print 'epx         ' , gpsd.fix.epx
-            print 'epv         ' , gpsd.fix.epv
-            print 'ept         ' , gpsd.fix.ept
-            print 'speed (m/s) ' , gpsd.fix.speed
-            print 'climb       ' , gpsd.fix.climb
-            print 'track       ' , gpsd.fix.track
-            print 'mode        ' , gpsd.fix.mode
-            print
-            print 'sats        ' , gpsd.satellites
+        time.sleep(5) #set to whatever
 
-            time.sleep(5) #set to whatever
-
-    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-        print "\nKilling Thread..."
-        gpsp.running = False
-        gpsp.join() # wait for the thread to finish what it's doing
-    print "Done.\nExiting."
+except (KeyboardInterrupt, SystemExit):
+    print "\nKilling Thread..."
+    gps_thread.running = False
+    gps_thread.join()
+print "Done.\nExiting."
